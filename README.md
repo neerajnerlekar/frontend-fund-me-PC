8888170ffc20457594ddd4aeec5d01a9e1da3b24
neerajnerlekar <neeraj.nerlekar@gmail.com>
2022-06-14 12:12:40 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Getting Started
+
+## Requirements
+
+-   [git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
+    -   You'll know you did it right if you can run `git --version` and you see a response like `git version x.x.x`
+-   [Nodejs](https://nodejs.org/en/)
+    -   You'll know you've installed nodejs right if you can run:
+        -   `node --version` and get an ouput like: `vx.x.x`
+-   [Yarn](https://yarnpkg.com/getting-started/install) instead of `npm`
+    -   You'll know you've installed yarn right if you can run:
+        -   `yarn --version` and get an output like: `x.x.x`
+        -   You might need to [install it with `npm`](https://classic.yarnpkg.com/lang/en/docs/install/) or `corepack`
+
+## Quickstart
+
+```shell
+git clone https://github.com/neerajnerlekar/hardhat-fund-me-PC
+cd hardhat-fund-me-PC
+yarn
+```
+
+Deploy:
+
+```shell
+yarn hardhat node
+```
+
+# Usage
+
+```shell
+git clone https://github.com/neerajnerlekar/frontend-fund-me-PC
+```
+
+# Connecting to the Contract
+
+-   Once you run the deploy the local hardhat node, change the `contractAddress` and `abi` in constants.js to the deployed address
+
+# Frontend
+
+-   Use extension like `Live Server` if in VS Code, or you can use `http-server` to host the `index.html`
+
+# Thank you!
+
+[![Neeraj Nerlekar Twitter](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/NeerajNerlekar)
+[![Neeraj Nerlekar Linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/nerlekarneeraj/)
---
+++ index.js
@@ -0,0 +1,82 @@
+// in front-end javascript you can't use require
+// you need to import ethers. So if not using React or NextJS the you can copy the ethers library locally and import it.
+
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+const balanceButton = document.getElementById("balanceButton")
+const withdrawButton = document.getElementById("withdrawButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+balanceButton.onclick = getBalance
+withdrawButton.onclick = withdraw
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        await ethereum.request({ method: "eth_requestAccounts" })
+        connectButton.innerHTML = "Connected!"
+    } else {
+        connectButton.innerHTML = "Please INSTALL Metamask!"
+    }
+}
+
+async function getBalance() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const balance = await provider.getBalance(contractAddress)
+        console.log(ethers.utils.formatEther(balance))
+    }
+}
+
+async function fund() {
+    const ethAmount = document.getElementById("ethAmount").value
+    console.log(`Funding with ${ethAmount}`)
+    if (typeof window.ethereum !== "undefined") {
+        // to send a transaction we absolutely need the following
+        // provider / connection to the blockchain
+        // signer / wallet / gas to pay for the transaction
+        // contract that we want to interact with - ABI & Address
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            })
+            await listenForTransactionMine(transactionResponse, provider)
+            console.log("Done!")
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+function listenForTransactionMine(transactionResponse, provider) {
+    console.log(`Mining ${transactionResponse.hash}.........`)
+    return new Promise((resolve, reject) => {
+        provider.once(transactionResponse.hash, (transactionReceipt) => {
+            console.log(
+                `Completed with ${transactionReceipt.confirmations} confirmations`
+            )
+            resolve()
+        })
+    })
+}
+
+async function withdraw() {
+    if (typeof window.ethereum !== "undefined") {
+        console.log("Withdrawing.....")
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdraw()
+            await listenForTransactionMine(transactionResponse, provider)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
